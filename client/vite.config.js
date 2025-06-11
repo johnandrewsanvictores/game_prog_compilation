1fa809998dd30f54354bf32f56f48b2d53846eb0
johnandrewsanvictores <sanvictoresjohnandrewe@gmail.com>
2025-06-11 14:02:55 +0800

    modify font

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig} from "vite";
-import {qrcode} from "vite-plugin-qrcode";
+import { defineConfig } from "vite";
+import { qrcode } from "vite-plugin-qrcode";
 
 export default defineConfig({
-    base: "./",
-    build: {
-        minify: "terser"
-    },
-    plugins: [qrcode()],
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-    },
-})
+  base: "./",
+  build: {
+    minify: "terser",
+  },
+  plugins: [qrcode()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
+});
